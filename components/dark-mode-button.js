a9d022b3ee6831ea002ebfd3023514a9a30a6166
Tino Reyna <tinoreyna1984@gmail.com>
2023-03-09 02:45:10 -0500

    refactoring

--- components/dark-mode-button.js
+++ components/dark-mode-button.js
@@ -1,10 +1,23 @@
-export default function DarkModeButton({ dmCallback }) {
+import { useDarkModeContext, useSetDarkModeContext } from "../utilities/dark-mode/context/dm-context";
+import { toggleTheme } from "../utilities/dark-mode/toggleTheme";
+
+export default function DarkModeButton() {
+    const darkMode = useDarkModeContext();
+    const setStoredMode = useSetDarkModeContext();
+
+    /* handles dark mode toggle */
+    const handleDarkMode = () => {
+        setStoredMode(toggleTheme(darkMode)); // changes storage variable value
+        document.body.classList.toggle("dark"); // adds/removes "dark" class
+    }
+
+
     return (
         <div className='md:mr-4 text-lg'>
             <label htmlFor="dark-mode" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
                 <span className="material-symbols-outlined"> light_mode </span>
                 <span className="relative">
-                    <input id="dark-mode" type="checkbox" onClick={dmCallback} className="hidden peer" />
+                    <input id="dark-mode" type="checkbox" onClick={handleDarkMode} className="hidden peer" />
                     <div className="w-10 h-6
                         rounded-full
                         shadow-inner
--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,7 @@
-import { useDarkModeContext, useSetDarkModeContext } from "../utilities/dark-mode/context/dm-context";
-import { toggleTheme } from "../utilities/dark-mode/toggleTheme";
 import Link from "next/link";
 import DarkModeButton from "./dark-mode-button";
 
 export default function Navbar() {
-
-    const darkMode = useDarkModeContext();
-    const setStoredMode = useSetDarkModeContext();
-
-    /* handles dark mode toggle */
-    const handleDarkMode = () => {
-        setStoredMode(toggleTheme(darkMode)); // changes storage variable value
-        document.body.classList.toggle("dark"); // adds/removes "dark" class
-    }
-
     return (
         <div
             className='flex flex-col md:flex-row md:justify-between md:items-baseline
@@ -27,7 +15,7 @@ export default function Navbar() {
                 <Link className='md:mr-4 text-lg dark:text-slate-200' href='#'>About</Link>
                 <Link className='md:mr-4 text-lg dark:text-slate-200' href='#'>Portfolio</Link>
                 <Link className='md:mr-4 text-lg dark:text-slate-200' href='#'>Contact</Link>
-                <DarkModeButton dmCallback={handleDarkMode} />
+                <DarkModeButton />
             </nav>
         </div>
     );

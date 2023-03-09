0a5f6ca1fc3b1e2507dc2b11870d4d892490a98e
Tino Reyna <tinoreyna1984@gmail.com>
2023-03-09 02:10:58 -0500

    minor fix

--- utilities/dark-mode/hooks/useTheme.js
+++ utilities/dark-mode/hooks/useTheme.js
@@ -1,6 +1,5 @@
 import { useLocalStorage } from "./useLocalStorage";
 
 export function useTheme() {
-    let theme = ((typeof window !== 'undefined') && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
-    return useLocalStorage('color-theme', theme);
+    return useLocalStorage('color-theme', 'light');
 }
